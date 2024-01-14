17e5e7e61562734f4d52d9e05f1edf7255e97c3f
Abhishek <90861021+Abhishek220801@users.noreply.github.com>
2024-01-15 01:29:31 +0530

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -43,10 +43,15 @@ export default function Footer() {
                                       Github
                                   </a>
                               </li>
-                              <li>
-                                  <Link to="/" className="hover:underline">
-                                      Discord
-                                  </Link>
+                              <li className="mb-4">
+                                  <a
+                                      href="https://www.linkedin.com/in/abhishek-sankhwar/"
+                                      className="hover:underline"
+                                      target="_blank"
+                                      rel="noreferrer"
+                                  >
+                                      LinkedIn
+                                  </a>
                               </li>
                           </ul>
                       </div>
@@ -70,9 +75,9 @@ export default function Footer() {
               <hr className="my-6 border-gray-200 sm:mx-auto lg:my-8" />
               <div className="sm:flex sm:items-center sm:justify-between">
                   <span className="text-sm text-gray-500 sm:text-center">
-                      © 2023
+                      © 2024
                       <a href="https://hiteshchoudhary.com/" className="hover:underline">
-                          hiteshchoudhary
+                          abhishek
                       </a>
                       . All Rights Reserved.
                   </span>
